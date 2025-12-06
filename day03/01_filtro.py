a535ac4afd89eed6f56b0bf118accf5a6e26a6b3
Matheus <matheus.fnds20@gmail.com>
2025-12-06 20:51:55 +0000

    filtros

--- day03/01_filtro.py
+++ day03/01_filtro.py
@@ -5,5 +5,5 @@ dfTransacoes = pd.read_csv("../data/transacoes.csv")
 dfTransacoes.head(2)
 
 # %% transacoes > 50 pontos
-filtro = dfTransacoes["QtdePontos"] > 50
-dfTransacoes[filtro].head(5)
+filtro = (dfTransacoes["QtdePontos"] > 50) & (dfTransacoes["QtdePontos"] < 500)
+dfTransacoes[filtro].head(2)
